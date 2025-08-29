0a89364921fdf6a01c4855de7bae1f25da622eee
matthew snoddy <djfdyuruiry@users.noreply.github.com>
2025-08-29 13:37:05 +0100

    feat(td): A* Path Finding Enhancement (#30)

--- scripts/generate-td-rules-doc/generate.py
+++ scripts/generate-td-rules-doc/generate.py
@@ -45,7 +45,7 @@ def main():
     root_dir = os.path.abspath(
         os.path.join(
             os.path.dirname(os.path.abspath(__file__)),
-            ".."
+            "../.."
         )
     )
 
