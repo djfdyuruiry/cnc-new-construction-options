fbd3f7862d3c02538526c8d9ec6a3139f17f6fd9
djfdyuruiry <djfdyuruiry@users.noreply.github.com>
2025-10-03 09:50:24 +0100

    chore(cmake): Remove deprecated options from clang format

--- cmake/ClangFormat.cmake
+++ cmake/ClangFormat.cmake
@@ -1,11 +1,5 @@
 if(CLANG_FORMAT_FOUND)
-    if(CLANG_FORMAT_VERSION VERSION_LESS "14.0.0")
-        message(WARNING "clang-format ${CLANG_FORMAT_VERSION} is older than 14.0.0, formatting may yield unexpected results")
-    elseif(CLANG_FORMAT_VERSION VERSION_GREATER "14.0.6")
-        message(WARNING "clang-format ${CLANG_FORMAT_VERSION} is newer than 14.0.6, formatting may yield unexpected results")
-    else()
-        message(STATUS "Found clang-format ${CLANG_FORMAT_VERSION}, 'format' target enabled")
-    endif()
+    message(STATUS "Found clang-format ${CLANG_FORMAT_VERSION}, 'format' target enabled")
 
     set(GLOB_PATTERNS
         common/*.cpp
