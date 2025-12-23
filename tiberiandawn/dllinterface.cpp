7a1cb34149e87bfe6e0eca1573ae09ebfdfab20f
djfdyuruiry <djfdyuruiry@users.noreply.github.com>
2025-12-23 20:27:02 +0000

    feat(td): Debug dll interface occupy

--- tiberiandawn/dllinterface.cpp
+++ tiberiandawn/dllinterface.cpp
@@ -373,6 +373,8 @@ public:
     }
 
 private:
+    static inline const auto Logger = CncLogger::For(DLLExportClass);
+
     static void Calculate_Single_Player_Score(EventCallbackStruct&);
 
     static unsigned int TD_Calculate_Efficiency(unsigned int harvested_credits,
@@ -4211,6 +4213,7 @@ bool DLLExportClass::Get_Sidebar_State(uint64 player_id, unsigned char* buffer_i
 
                             if (sidebar_entry.Completed && sidebar_entry.Type == BUILDING_TYPE) {
                                 if (tech) {
+                                    // TODO: Here is where remastered determines occupy shape for completed building
                                     BuildingTypeClass* building_type = (BuildingTypeClass*)tech;
                                     short const* occupy_list = building_type->Occupy_List(true);
                                     if (occupy_list) {
@@ -4222,6 +4225,8 @@ bool DLLExportClass::Get_Sidebar_State(uint64 player_id, unsigned char* buffer_i
                                             occupy_list++;
                                         }
                                     }
+
+                                    CNC_LOGGER_INFO("Exporting sidebar state for '{}' - occupy length: {}", building_type->IniName, sidebar_entry.PlacementListLength);
                                 }
                             }
                         }
