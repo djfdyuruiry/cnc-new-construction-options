4ff8c241d5235428e3a32fda67c8877277d048f5
djfdyuruiry <djfdyuruiry@users.noreply.github.com>
2026-05-02 20:34:12 +0100

    fix(td): Enable lua console to handle any return type

--- common/lua/luaengine.cpp
+++ common/lua/luaengine.cpp
@@ -424,8 +424,42 @@ LuaEvalResult LuaEngine::Eval(const std::string& expression) const
 
 LuaResultWithValue<std::string> LuaEngine::Eval_To_String(const std::string& expression) const
 {
-    if (const auto result = Eval(expression); !result.Is_Ok() || !result.Returned_Value()) {
-        return { result };
+    CNC_LOGGER_TRACE("Attempting to evaluate lua expression as string: {}", expression);
+
+    auto eval_result = Get_Value_From_State<LuaEvalResult>([&expression](auto L)
+    {
+        // attempt to compile an evaluation first
+        const auto return_expression = std::format("return tostring({})", expression);
+
+        auto status = luaL_loadstring(L, return_expression.c_str());
+        const auto eval_returns_value = status == LUA_OK;
+
+        if (!eval_returns_value) {
+            // expression cannot be evaluated, just compile for execution instead
+            status = luaL_loadstring(L, expression.c_str());
+        }
+
+        if (status != LUA_OK) {
+            auto result = LuaResult(L, status);
+
+            CNC_LOGGER_TRACE(
+                "Error loading lua script due to '{}' error: {}",
+                result.Code_As_String(),
+                result.Error_Message()
+            );
+            return LuaEvalResult(result, eval_returns_value);
+        }
+
+        const auto eval_result = LuaResult(
+            L,
+            lua_pcall(L, 0, LUA_MULTRET, 0)
+        );
+
+        return LuaEvalResult(eval_result, eval_returns_value);
+    });
+
+    if (!eval_result.Is_Ok() || !eval_result.Returned_Value()) {
+        return { eval_result };
     }
 
     return To_String();
