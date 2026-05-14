bc4796ff70a083dfc934fc2f2c112a0f3bb27431
matthew snoddy <djfdyuruiry@users.noreply.github.com>
2026-05-14 20:34:23 +0100

    fix(common): Escape json chars in log file (#86)

--- common/logger.cpp
+++ common/logger.cpp
@@ -3,6 +3,71 @@
 #include "logger.h"
 #include "paths.h"
 
+/**
+ * Wrapper around @class{spdlog::sinks::rotating_file_sink_mt}
+ *
+ * Sanitizes log messages before passing them down to the concrete sink, ensuring characters
+ * are escaped and string is safe to use in a JSON string.
+ */
+class JsonSafeRotatingFileSink final : public spdlog::sinks::sink
+{
+public:
+    JsonSafeRotatingFileSink(spdlog::filename_t base_filename, const std::size_t max_size, const std::size_t max_files)
+    {
+        FileSink = std::make_unique<spdlog::sinks::rotating_file_sink_mt>(
+            std::move(base_filename),
+            max_size,
+            max_files
+        );
+    }
+
+    void log(const spdlog::details::log_msg& msg) override
+    {
+        std::string sanitized_payload;
+
+        for (const auto& c : msg.payload) {
+            if (auto sanitize_pair = SanitizeMap.find(c); sanitize_pair != SanitizeMap.end()) {
+                sanitized_payload += sanitize_pair->second;
+            } else {
+                sanitized_payload += c;
+            }
+        }
+
+        const auto sanitized_message = spdlog::details::log_msg(
+            msg.time,
+            msg.source,
+            msg.logger_name,
+            msg.level,
+            sanitized_payload
+        );
+
+        FileSink->log(sanitized_message);
+    }
+
+    void flush() override
+    {
+        FileSink->flush();
+    }
+
+    void set_pattern(const std::string& pattern) override
+    {
+        FileSink->set_pattern(pattern);
+    }
+
+    void set_formatter(std::unique_ptr<spdlog::formatter> sink_formatter) override
+    {
+        FileSink->set_formatter(std::move(sink_formatter));
+    }
+
+private:
+    static inline const std::unordered_map<char, std::string> SanitizeMap {
+        {'\n', "\\n"},
+        {'"', "\\\""}
+    };
+
+    std::unique_ptr<spdlog::sinks::rotating_file_sink_mt> FileSink;
+};
+
 const CncLogger& CncLogger::Default()
 {
     const static auto default_logger = CncLogger(DefaultLoggerName);
@@ -67,15 +132,15 @@ void CncLogger::Init_SpdLog()
         spdlog::set_level(spdlog::level::err);
     }
 
+    Sinks.clear();
+
     spdlog::init_thread_pool(8192, 1);
 
-    Sinks = std::vector<spdlog::sink_ptr>();
-
     // console logging
-    StdoutSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-    StdoutSink->set_pattern("%^%L [%!]%$ %v");
+    auto stdout_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    stdout_sink->set_pattern("%^%L [%=10!n] %v%$");
 
-    Sinks.emplace_back(StdoutSink);
+    Sinks.emplace_back(std::move(stdout_sink));
 
     // create log file in user path, filename matches program binary (nco-td.log, TIBERIANDAWN.DLL.log etc.)
     const auto log_file_name = std::format("{}.log", PathsClass::Try_Get_Program_Binary_Name());
@@ -84,18 +149,18 @@ void CncLogger::Init_SpdLog()
         .append(log_file_name)
         .string();
 
-    RotatingSink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+    auto rotating_sink = std::make_shared<JsonSafeRotatingFileSink>(
         log_file,
         1024 * 1024 * 10,
         3
     );
-    RotatingSink->set_pattern( // BUG: Newlines in message strings break JSONL format
-        R"({ "time": "%Y-%m-%dT%H:%M:%S.%f%z", "name": "%n", "level": "%^%l%$", "at": "%@", "in": "%!", "process": %P, "thread": %t, "message": "%v" })"
+    rotating_sink->set_pattern(
+        R"({ "time": "%T.%e", "level": "%L", "message": "%v", "logger": "%n", "at": "%s:%#", "in": "%!", "date": "%d-%m-%C", "process": %P, "thread": %t })"
     );
 
-    Sinks.emplace_back(RotatingSink);
+    Sinks.emplace_back(std::move(rotating_sink));
 
-    // default logger for Debug_String_Log legacy support
+    // default logger for Debug_String_Log legacy support, see debugstring.cpp
     spdlog::set_default_logger(
         Build_Logger(DefaultLoggerName)
     );
