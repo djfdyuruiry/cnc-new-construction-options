05eafa1ea59076ab6c0479aad7c760040989c943
djfdyuruiry <djfdyuruiry@users.noreply.github.com>
2026-05-20 17:09:31 +0100

    feat(td): Make credits tab placement relative to sidebar location

--- tiberiandawn/tab.cpp
+++ tiberiandawn/tab.cpp
@@ -127,7 +127,7 @@ void TabClass::Draw_It(bool complete)
 
 void TabClass::Draw_Credits_Tab(void)
 {
-    unsigned x = Get_Resolution_Factor() ? 320 : 160;
+    const unsigned x = Get_Resolution_Factor() ? Map.SideX - Get_Shape_Size(TabShape) : 160;
     CC_Draw_Shape(TabShape, 0, x, 0, WINDOW_MAIN, SHAPE_NORMAL);
 }
 
