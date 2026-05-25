7c5005e32388bda5c16e73a92a6d4eae7f122069
djfdyuruiry <djfdyuruiry@users.noreply.github.com>
2026-05-25 03:43:18 +0100

    feat(common): Texture class for cached texture fills

---
+++ common/texture.cpp
@@ -0,0 +1,80 @@
+#include "texture.h"
+
+#include "ccfile.h"
+#include "keyframe.h"
+
+#define SHAPE_BUFFER_SIZE 131072L
+
+void* Get_Shape_Header_Data(void* ptr);
+
+static inline const auto& Logger = CncLogger::For(Texture);
+
+Texture::Texture(std::string shape_filename, const unsigned short shape_number)
+    : ShapeFilename(std::move(shape_filename))
+    , ShapeNumber(shape_number)
+    , Loaded(false)
+    , ShapeData(std::make_unique<unsigned char[]>(SHAPE_BUFFER_SIZE))
+    , ShapeHeader(nullptr)
+    , ShapeWidth(0)
+    , ShapeHeight(0)
+{
+}
+
+bool Texture::Load()
+{
+    if (Loaded) {
+        return true;
+    }
+
+    CNC_LOGGER_DEBUG("Loading texture from file: {} (index={})", ShapeFilename, ShapeNumber);
+
+    const auto shape_file = MixFileClass<CCFileClass>::Retrieve(ShapeFilename.c_str());
+    const auto shape_size = Build_Frame(shape_file, ShapeNumber, ShapeData.get());
+
+    const auto shape_buffer_overflow = Get_Last_Frame_Length() > SHAPE_BUFFER_SIZE;
+
+    if (shape_buffer_overflow) {
+        CNC_LOGGER_WARN(
+            "Attempted to load shape bigger than buffer: {} > {}",
+            Get_Last_Frame_Length(),
+            SHAPE_BUFFER_SIZE
+        );
+    }
+
+    if (shape_buffer_overflow || shape_size == 0) {
+        CNC_LOGGER_ERROR("Failed to load shape from file: {} (index={})", ShapeFilename, ShapeNumber);
+        return false;
+    }
+
+    ShapeHeader = static_cast<unsigned char*>(Get_Shape_Header_Data(reinterpret_cast<void*>(shape_size)));
+    ShapeWidth = Get_Build_Frame_Width(shape_file);
+    ShapeHeight = Get_Build_Frame_Height(shape_file);
+
+    Loaded = true;
+
+    return true;
+}
+
+void Texture::Draw_Rectangle(
+    GraphicViewPortClass& viewport,
+    const int x,
+    const int y,
+    const int width,
+    const int height
+) const
+{
+    if (!Loaded) {
+        CNC_LOG_WARN("Attempted to draw texture before it was successfully loaded");
+        return;
+    }
+
+    viewport.Texture_Fill_Rect(
+        x,
+        y,
+        width,
+        height,
+        ShapeHeader,
+        ShapeWidth,
+        ShapeHeight
+    );
+}
---
+++ common/texture.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "graphicsviewport.h"
+
+#include <memory>
+#include <string>
+
+/**
+ * Stores a texture in memory and provides convenience methods for drawing the texture onto a viewport.
+ *
+ * Call Load at least once before calling Draw, load must return true.
+ *
+ * OOP version of CC_Texture_Fill from Tiberian Dawn with caching.
+ */
+class Texture
+{
+public:
+    Texture(std::string shape_filename, unsigned short shape_number);
+
+    /**
+     * Attempt to load the data for this texture from Texture::ShapeFilename.
+     *
+     * This method is idempotent.
+     *
+     * @return Is the texture loaded?
+     */
+    bool Load();
+
+    /**
+     * Draw this texture to a rectangle on the given viewport, with the target dimensions.
+     *
+     * Texture::Load must have been called and return true, otherwise this method will do nothing.
+     */
+    void Draw_Rectangle(GraphicViewPortClass& viewport, int x, int y, int width, int height) const;
+
+private:
+    // source info
+    std::string ShapeFilename;
+    unsigned short ShapeNumber;
+
+    // texture state
+    bool Loaded;
+
+    // shape file data
+    std::unique_ptr<unsigned char[]> ShapeData;
+    unsigned char* ShapeHeader;
+
+    // shape header data
+    int ShapeWidth;
+    int ShapeHeight;
+};
