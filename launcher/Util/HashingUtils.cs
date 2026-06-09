12e3c72e2c6ad1b2cd8839c38601217bd4c97aa5
matthew snoddy <djfdyuruiry@users.noreply.github.com>
2026-06-09 21:55:25 +0100

    feat(launcher): Caching for zip url files (#120)

--- launcher/Util/HashingUtils.cs
+++ launcher/Util/HashingUtils.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Runtime.Versioning;
 using System.Threading.Tasks;
 
@@ -51,6 +52,59 @@ public static class HashingUtils
       // cleanup corrupt file
       await Console.Error.WriteLineAsync($"WARN Purging corrupted file due to mismatched checksum: {filePath}");
       File.Delete(filePath);
+    }
+
+    return isValid;
+  }
+
+  public static async Task<Hash> CalculateDirectoryChecksum(
+    string path,
+    string searchPattern,
+    int bufferSize = 4096 * 32
+  )
+  {
+    using var hasher = Hasher.New();
+
+    foreach (var matchedFile in Directory.GetFiles(path, searchPattern).Order())
+    {
+      await using var stream = File.OpenRead(matchedFile);
+
+      var buffer = new byte[bufferSize];
+      int bytesRead;
+
+      while ((bytesRead = await stream.ReadAsync(buffer)) != 0)
+      {
+        hasher.Update(buffer.AsSpan(start: 0, bytesRead));
+      }
+    }
+
+    return hasher.Finalize();
+  }
+
+  public static async Task<bool> DirectoryChecksumIsValid(
+    string path,
+    string searchPattern,
+    string expectedChecksum,
+    bool deleteInvalidFiles = false
+  )
+  {
+    if (!Directory.Exists(path))
+    {
+      return false;
+    }
+
+    var directoryHash = await CalculateDirectoryChecksum(path, searchPattern);
+    var isValid = string.Equals(
+      directoryHash.ToString(),
+      expectedChecksum,
+      StringComparison.OrdinalIgnoreCase
+    );
+
+    if (!isValid && deleteInvalidFiles)
+    {
+      // cleanup corrupt directory
+      await Console.Error.WriteLineAsync($"WARN Purging corrupted directory due to mismatched checksum: {path}");
+      Directory.Delete(path, true);
     }
 
     return isValid;
