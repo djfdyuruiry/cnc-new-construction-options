4a2cd706e56005a2a594c63a311ce143c1f69a49
djfdyuruiry <djfdyuruiry@users.noreply.github.com>
2026-06-16 02:36:25 +0100

    fix(td): String utils format missing header

--- common/stringutils.cpp
+++ common/stringutils.cpp
@@ -1,5 +1,4 @@
 #include <algorithm>
-#include <format>
 
 #include "stringutils.h"
 
--- common/stringutils.h
+++ common/stringutils.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <format>
 #include <functional>
 #include <sstream>
 #include <string>
