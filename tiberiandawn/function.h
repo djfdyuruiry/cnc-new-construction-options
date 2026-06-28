188b97a344596139d1f29a18de341696062b9543
djfdyuruiry <djfdyuruiry@users.noreply.github.com>
2026-06-28 15:50:10 +0100

    feat(td): Rule overrides in SpecialClass

--- tiberiandawn/function.h
+++ tiberiandawn/function.h
@@ -491,7 +491,7 @@ void Set_Scenario_Name(char* buf,
                        ScenarioDirType dir = SCEN_DIR_NONE,
                        ScenarioVarType var = SCEN_VAR_NONE);
 void Write_Scenario_Ini(char* root);
-bool Read_Scenario_Ini(char* root, const SpecialClass& special_options, bool init_rules = true, bool init_lua = true, std::optional<bool> allow_superweapons = std::nullopt, bool fresh = true);
+bool Read_Scenario_Ini(char* root, const SpecialClass& special_options, const bool init_rules, const bool init_lua, const std::optional<bool> allow_superweapons, const bool fresh)
 bool Read_Scenario_Ini_File(char* scenario_file_name, char* bin_file_name, const char* root, bool fresh);
 int Scan_Place_Object(ObjectClass* obj, CELL cell);
 
