d6f4688f6736df1b472f7e4a7fa83e3c0ed7ec46
djfdyuruiry <djfdyuruiry@users.noreply.github.com>
2026-07-01 22:25:31 +0100

    feat(td): Base class for OOP dialog approach

---
+++ common/dialog.h
@@ -0,0 +1,246 @@
+#pragma once
+#include "framelimit.h"
+#include "gadget.h"
+
+struct DialogControlDimension
+{
+    int X = 0;
+    int Y = 0;
+    int W = 0;
+    int H = 0;
+};
+
+template<typename T> concept EnumerationType = std::is_enum_v<T>;
+
+template<EnumerationType T>
+class Dialog
+{
+protected:
+    /*........................................................................
+    Redraw values: in order from "top" to "bottom" layer of the dialog
+    ........................................................................*/
+    typedef enum
+    {
+        REDRAW_NONE = 0,
+        REDRAW_FOREGROUND, // dynamic text, custom control-like objects
+        REDRAW_CONTROLS, // controls
+        REDRAW_BACKGROUND, // static text, decorations
+        REDRAW_ALL = REDRAW_BACKGROUND
+    } DialogRedrawType;
+
+    // base dimensions (not scaled)
+
+    int BaseWidth;
+    int BaseHeight;
+    int BaseMarginWidth;
+    int BaseMarginHeight;
+
+    // display scale
+    int Factor;
+
+    // dimensions (scaled)
+
+    int X;
+    int Y;
+    int Width;
+    int Height;
+    int Center;
+
+    int TextHeight;
+    int MarginWidth;
+    int MarginHeight;
+
+    // button shapes
+
+    void const* UpButtonShape;
+    void const* DownButtonShape;
+
+    // controls
+
+    std::map<T, DialogControlDimension> Dimensions;
+    std::map<T, std::unique_ptr<char[]>> Text;
+    std::map<T, std::unique_ptr<GadgetClass>> Controls;
+    GadgetClass* CommandChain;
+
+    bool Is_Mouse_Over_Rectangle(const int start_x, const int start_y, const int end_x, const int end_y)
+    {
+        return Keyboard->MouseQX >= start_x
+            && Keyboard->MouseQX <= end_x
+            && Keyboard->MouseQY >= start_y
+            && Keyboard->MouseQY <= end_y;
+    }
+
+    bool Is_Mouse_Outside_Control_Dimensions(const GadgetClass& control)
+    {
+        return (Keyboard->MouseQX < control.X || Keyboard->MouseQX > control.X + control.Width)
+            && (Keyboard->MouseQY < control.Y || Keyboard->MouseQY > control.Y + control.Height);
+    }
+
+    template<std::derived_from<GadgetClass> U>
+    U& Get_Control(const T type)
+    {
+        return *reinterpret_cast<U*>(Controls[type].get());
+    }
+
+    template<std::derived_from<GadgetClass> U, typename... Args>
+    U& Add_Control(const T type, Args&&... args)
+    {
+        Controls[type] = std::make_unique<U>(type, std::forward<Args>(args)...);
+
+        auto& control = Get_Control<U>(type);
+
+        // add control to command chain so we can capture input
+        if (CommandChain == nullptr) {
+            // first element in chain
+            CommandChain = &control;
+        } else {
+            // nth element in chain
+            control.Add_Tail(*CommandChain);
+        }
+
+        return control;
+    }
+
+    template<T type, std::derived_from<GadgetClass> U, typename... Args>
+    U& Add_Control(Args&&... args)
+    {
+        return Add_Control<U>(type, std::forward<Args>(args)...);
+    }
+
+    virtual std::optional<bool> On_Input(DialogRedrawType& display, KeyNumType& input) = 0;
+
+    virtual KeyNumType Get_Input(DialogRedrawType& display)
+    {
+        return CommandChain != nullptr ? CommandChain->Input() : KN_NONE;
+    }
+
+    virtual void Render_Foreground(DialogRedrawType& display) = 0;
+
+    virtual void Render_Background(DialogRedrawType& display)
+    {
+        Dialog_Box(X, Y, Width, Height);
+
+        // init font variables
+        Fancy_Text_Print(
+            TXT_NONE, 0, 0, TBLACK, TBLACK, TPF_CENTER | TPF_6PT_GRAD | TPF_USE_GRAD_PAL | TPF_NOSHADOW
+        );
+
+        Draw_Caption(TXT_NONE, X, Y, Width);
+    }
+
+    void Render(DialogRedrawType& display)
+    {
+        if (!display) {
+            return;
+        }
+
+        Hide_Mouse();
+
+        if (display >= REDRAW_BACKGROUND) {
+            Render_Background(display);
+        }
+
+        if (display >= REDRAW_FOREGROUND) {
+            Render_Foreground(display);
+        }
+
+        if (display >= REDRAW_CONTROLS && CommandChain != nullptr) {
+            CommandChain->Flag_List_To_Redraw();
+        }
+
+        Show_Mouse();
+        display = REDRAW_NONE;
+    }
+
+    virtual void Init_UI_State() = 0;
+    virtual void Init_Data() = 0;
+
+    virtual void Init_Controls()
+    {
+        Text.clear();
+        Controls.clear();
+        CommandChain = nullptr;
+    }
+
+    virtual void Init_Shapes()
+    {
+        if (InMainLoop || Factor == 1) {
+            UpButtonShape = UpButtonShape == nullptr ? Hires_Retrieve("BTN-UP.SHP") : UpButtonShape;
+            DownButtonShape = DownButtonShape == nullptr ? Hires_Retrieve("BTN-DN.SHP") : DownButtonShape;
+        } else {
+            UpButtonShape = UpButtonShape == nullptr ? Hires_Retrieve("BTN-UP2.SHP") : UpButtonShape;
+            DownButtonShape = DownButtonShape == nullptr ? Hires_Retrieve("BTN-DN2.SHP") : DownButtonShape;
+        }
+    }
+
+    virtual void Init_Dimensions(const int screen_width, const int screen_height, const int factor)
+    {
+        Factor = factor;
+
+        Width = BaseWidth * Factor;
+        Height = BaseHeight * Factor;
+        X = (screen_width - Width) / 2;
+        Y = (screen_height - Height) / 2;
+        Center = X + (Width / 2);
+
+        TextHeight = 6 * Factor + 1; // ht of 6-pt text
+        MarginWidth = BaseMarginWidth * Factor;
+        MarginHeight = BaseMarginHeight * Factor;
+
+        Dimensions.clear();
+    }
+
+    Dialog(
+        const int width,
+        const int height,
+        const int margin_width,
+        const int margin_height
+    ) : BaseWidth(width),
+        BaseHeight(height),
+        BaseMarginWidth(margin_width),
+        BaseMarginHeight(margin_height),
+        UpButtonShape(nullptr),
+        DownButtonShape(nullptr),
+        CommandChain(nullptr)
+    {}
+    virtual ~Dialog() = default;
+
+public:
+    virtual void Init(const int screen_width, const int screen_height, const int factor)
+    {
+        Init_Dimensions(screen_width, screen_height, factor);
+        Init_Shapes();
+        Init_Controls();
+        Init_Data();
+        Init_UI_State();
+    }
+
+    virtual bool Present()
+    {
+        auto display = REDRAW_ALL; // redraw level
+
+        while (true) {
+            /*
+            ** If we have just received input focus again after running in the background then
+            ** we need to redraw.
+            */
+            if (AllSurfaces.SurfacesRestored) {
+                AllSurfaces.SurfacesRestored = false;
+                display = REDRAW_ALL;
+            }
+
+            Call_Back();
+            Render(display);
+
+            auto input = Get_Input(display);
+
+            auto result = On_Input(display, input);
+
+            if (result.has_value()) {
+                return *result;
+            }
+
+            Frame_Limiter();
+        }
+    }
+};
