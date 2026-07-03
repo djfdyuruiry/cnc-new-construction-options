fb700e8bcd06ca0042283dab96e7192e54f908ab
djfdyuruiry <djfdyuruiry@users.noreply.github.com>
2026-07-03 01:48:55 +0100

    feat(td): Starting Rules Editor

--- tiberiandawn/ruleseditordlg.cpp
+++ tiberiandawn/ruleseditordlg.cpp
@@ -9,11 +9,13 @@ typedef enum
     SECTION_DROPDOWN,
     SEARCH_TEXTBOX,
     SEARCH_BUTTON,
-    LEFT_RULE_VALUE_CONTROL, // 12 controls
-    LEFT_RULE_HELP_CONTROL = 210,
-    RIGHT_RULE_VALUE_CONTROL = 216,  // 12 controls
-    RIGHT_RULE_HELP_CONTROL = 222,
-    PREVIOUS_BUTTON = 228,
+    LEFT_PANEL, // virtual panel used for drawing
+    LEFT_RULE_VALUE_CONTROL, // 6 controls
+    LEFT_RULE_HELP_CONTROL = 211, // 6 controls
+    RIGHT_PANEL = 217, // virtual panel used for drawing
+    RIGHT_RULE_VALUE_CONTROL,  // 6 controls
+    RIGHT_RULE_HELP_CONTROL = 223, // 6 controls
+    PREVIOUS_BUTTON = 229,
     NEXT_BUTTON,
     EXIT_BUTTON
 } RulesEditorControls;
@@ -22,7 +24,7 @@ class RulesEditorDialog : public Dialog<RulesEditorControls>
 {
     static constexpr auto DropdownTextLength = 25;
 
-    void Init_Bottom_row()
+    void Init_Bottom_Row()
     {
         Add_Button(PREVIOUS_BUTTON, "Previous");
         Add_Button(NEXT_BUTTON, "Next");
@@ -31,12 +33,10 @@ class RulesEditorDialog : public Dialog<RulesEditorControls>
 
     void Init_Right_Rules_Panel()
     {
-
     }
 
     void Init_Left_Rules_Panel()
     {
-
     }
 
     void Init_Top_Row()
@@ -82,6 +82,16 @@ class RulesEditorDialog : public Dialog<RulesEditorControls>
         );
     }
 
+    int ControlWidth;
+    int ControlHeight;
+    int DropdownItemWidth;
+    int HorizontalSpacing;
+    int VerticalSpacing;
+
+    int ControlsX;
+    int TopRowY;
+    int MiddleRowY;
+    int BottomRowY;
 protected:
     std::optional<bool> On_Input(DialogRedrawType& display, KeyNumType& input) override
     {
@@ -106,25 +116,22 @@ protected:
     {
         Dialog::Render_Background(display);
 
-        const auto panel_width = (Width - (MarginWidth * 2)) / 2;
-        const auto control_height = 9 * Factor;
-
         // left panel border
         LogicPage->Draw_Rect(
-            X + MarginWidth,
-            Dimensions[FILE_DROPDOWN].Y + control_height + (2 * Factor),
-            X + Width - MarginWidth - panel_width - 1 - 2,
-            Dimensions[NEXT_BUTTON].Y - (2 * Factor),
-            LTGREEN
+            Dimensions[LEFT_PANEL].X,
+            Dimensions[LEFT_PANEL].Y,
+            Dimensions[LEFT_PANEL].X + Dimensions[LEFT_PANEL].W,
+            Dimensions[LEFT_PANEL].Y + Dimensions[LEFT_PANEL].H,
+            LTGRAY
         );
 
         // right panel border
         LogicPage->Draw_Rect(
-            X + Width - MarginWidth - panel_width + 1,
-            Dimensions[FILE_DROPDOWN].Y + control_height + (2 * Factor),
-            X + Width - MarginWidth - 2,
-            Dimensions[NEXT_BUTTON].Y - (2 * Factor),
-            LTGREEN
+            Dimensions[RIGHT_PANEL].X,
+            Dimensions[RIGHT_PANEL].Y,
+            Dimensions[RIGHT_PANEL].X + Dimensions[RIGHT_PANEL].W,
+            Dimensions[RIGHT_PANEL].Y + Dimensions[RIGHT_PANEL].H,
+            LTGRAY
         );
     }
 
@@ -160,65 +167,86 @@ protected:
         Init_Top_Row();
         Init_Left_Rules_Panel();
         Init_Right_Rules_Panel();
-        Init_Bottom_row();
+        Init_Bottom_Row();
     }
 
     void Init_Dimensions(const int screen_width, const int screen_height, const int factor) override
     {
         Dialog::Init_Dimensions(screen_width, screen_height, factor);
 
-        const auto control_width = 40 * Factor;
-        const auto control_height = 9 * Factor;
-        const auto dropdown_item_width = 8 * Factor;
-        const auto horizontal_spacing = 3 * Factor;
-        const auto vertical_spacing = 2 * Factor;
+        ControlWidth = 40 * Factor;
+        ControlHeight = 9 * Factor;
+        DropdownItemWidth = 8 * Factor;
+        HorizontalSpacing = 3 * Factor;
+        VerticalSpacing = 2 * Factor;
 
-        const auto top_row_x = X + MarginWidth;
-        const auto top_row_y = Y + MarginHeight + (13 * Factor);
+        ControlsX = X + MarginWidth;
+        TopRowY = Y + MarginHeight + (13 * Factor);
+        MiddleRowY = TopRowY + ControlHeight + VerticalSpacing;
+        BottomRowY = Y + Height - ControlHeight - MarginHeight;
 
+        // top row
         Dimensions[FILE_DROPDOWN]= {
-            top_row_x,
-            top_row_y,
-            static_cast<int>(nearbyint(control_width * 1.2)),
-            5 * dropdown_item_width // 5 visible items (excluding selected value)
+            ControlsX,
+            TopRowY,
+            static_cast<int>(nearbyint(ControlWidth * 1.2)),
+            5 * DropdownItemWidth // 5 visible items (excluding selected value)
         };
 
         Dimensions[SECTION_DROPDOWN] = {
-            top_row_x + Dimensions[FILE_DROPDOWN].W + (horizontal_spacing * 4),
-            top_row_y,
-            static_cast<int>(nearbyint(control_width * 1.75)),
-            10 * dropdown_item_width // 10 visible items (excluding selected value)
+            ControlsX + Dimensions[FILE_DROPDOWN].W + (HorizontalSpacing * 4),
+            TopRowY,
+            static_cast<int>(nearbyint(ControlWidth * 1.75)),
+            10 * DropdownItemWidth // 10 visible items (excluding selected value)
         };
 
         Dimensions[SEARCH_BUTTON] = {
-            X + Width - control_width - MarginWidth,
-            top_row_y,
-            control_width,
-            control_height
+            X + Width - ControlWidth - MarginWidth,
+            TopRowY,
+            ControlWidth,
+            ControlHeight
         };
 
-        Dimensions[SEARCH_TEXTBOX].X = Dimensions[SEARCH_BUTTON].X - (control_width * 2) - horizontal_spacing,
-        Dimensions[SEARCH_TEXTBOX].Y = top_row_y;
-        Dimensions[SEARCH_TEXTBOX].W = control_width * 2;
-        Dimensions[SEARCH_TEXTBOX].H = control_height;
+        Dimensions[SEARCH_TEXTBOX] = {
+            Dimensions[SEARCH_BUTTON].X - (ControlWidth * 2) - HorizontalSpacing,
+            TopRowY,
+            ControlWidth * 2,
+            ControlHeight
+        };
 
-        Dimensions[EXIT_BUTTON].X = X + Width - control_width - MarginWidth;
-        Dimensions[EXIT_BUTTON].Y = Y + Height - control_height - MarginHeight;
-        Dimensions[EXIT_BUTTON].W = control_width;
-        Dimensions[EXIT_BUTTON].H = control_height;
+        // bottom row
+        Dimensions[EXIT_BUTTON] = {ControlsX, BottomRowY, ControlWidth, ControlHeight};
 
         Dimensions[NEXT_BUTTON] = {
-            Dimensions[EXIT_BUTTON].X,
-            Dimensions[EXIT_BUTTON].Y - control_height - vertical_spacing,
-            control_width,
-            control_height
+            X + Width - ControlWidth - MarginWidth,
+            BottomRowY,
+            ControlWidth,
+            ControlHeight
         };
 
         Dimensions[PREVIOUS_BUTTON] = {
-            Dimensions[NEXT_BUTTON].X - control_width - horizontal_spacing,
-            Dimensions[NEXT_BUTTON].Y,
-            control_width,
-            control_height
+            Dimensions[NEXT_BUTTON].X - ControlWidth - HorizontalSpacing,
+            BottomRowY,
+            ControlWidth,
+            ControlHeight
+        };
+
+        // panels (middle row)
+        const auto panel_width = ((Width - (MarginWidth * 2)) / 2) - VerticalSpacing;
+        const auto panel_height = Dimensions[NEXT_BUTTON].Y - MiddleRowY - VerticalSpacing;
+
+        Dimensions[LEFT_PANEL] = {
+            ControlsX,
+            MiddleRowY,
+            panel_width,
+            panel_height
+        };
+
+        Dimensions[RIGHT_PANEL] = {
+            X + Width - MarginWidth - panel_width,
+            MiddleRowY,
+            panel_width,
+            panel_height
         };
     }
 
